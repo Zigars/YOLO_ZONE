979b55dc3107f6d3abf51b4301a3477aa526dbd4
Zigars <1370170007@qq.com>
2021-11-03 14:22:39 +0800

    summary update

--- models/summary.py
+++ models/summary.py
@@ -9,7 +9,7 @@ from torchsummary import summary
 from yolox import YOLOX
 
 if __name__ == '__main__':
-    # 需要使用device来指定网络在GPU还是CPU运行
+    # cuda or cpu
     device  = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     m       = YOLOX(80, "nano").to(device)
     
